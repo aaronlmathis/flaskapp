b76e496809e94a4eb674cb3ae7ff6f4fd377831b
EC2 Default User <ec2-user@ip-172-31-40-201.ec2.internal>
2024-07-28 16:45:03 +0000

    basic app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import platform
+
 application = Flask(__name__)
 
 @application.route("/")
+
+
 def index():
 	return "Nobody expects the spanish inquisition!"
+
+@application.route("/python-version")
+def python_version():
+	python_version = platform.python_version()
+	return f'The python version running on this host is: {python_version}'
 
 if __name__ == "__main__":
 	application.run(host='0.0.0.0', port='8080')
