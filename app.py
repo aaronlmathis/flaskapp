cd9cf89edb6d189b200b1c4a967c9a0725bf5d04
EC2 Default User <ec2-user@ip-172-31-40-201.ec2.internal>
2024-07-28 17:18:52 +0000

    basic template on app

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-from flask import Flask
-import platform
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 @application.route("/")
-
-
 def index():
-	return "Nobody expects the spanish inquisition!"
-
-@application.route("/python-version")
-def python_version():
-	python_version = platform.python_version()
-	return f'The python version running on this host is: {python_version}'
+    return render_template('index.html')
 
 if __name__ == "__main__":
-	application.run(host='0.0.0.0', port='8080')
-
-
+    application.run(host='0.0.0.0', port='8080')
