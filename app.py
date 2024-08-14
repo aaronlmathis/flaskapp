a4fcb25be6a20ad0aa62451e2a1af90c7938554f
Aaron Mathis <aaron.mathis@gmail.com>
2024-08-14 11:14:08 -0500

    initial

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='8080')
+@app.route('/test')
+def test():
+    return render_template("test.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
