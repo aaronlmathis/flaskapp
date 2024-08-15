59a4bdb3a8015d313c99ac8b0eb44aef91dd2efc
Aaron Mathis <aaron.mathis@gmail.com>
2024-08-15 12:24:32 -0500

    misc

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
-from flask import Flask, redirect, url_for, render_template
+from website import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-@app.route('/test')
-def test():
-    return render_template("test.html")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
